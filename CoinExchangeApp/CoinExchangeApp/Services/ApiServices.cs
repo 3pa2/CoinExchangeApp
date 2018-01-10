7a548b8190cc4b2da7fdf10c7616c22e6187c0b2
victor brito <v.brito@onclick.studio>
2018-01-09 19:56:17 -0800

    first funtion

--- CoinExchangeApp/CoinExchangeApp/Services/ApiServices.cs
+++ CoinExchangeApp/CoinExchangeApp/Services/ApiServices.cs
@@ -1,11 +1,60 @@
-﻿using System;
+﻿using CoinExchangeApp.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CoinExchangeApp.Services
 {
     public class ApiServices
     {
+        //https://www.coinexchange.io/api/v1/
+        private const string URLBASE = "https://www.coinexchange.io/api/{0}/{1}";
+        public async Task<Response> Get<T>(string endPoint, string prefixToken = null,string accessToken = null)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(URLBASE);
 
+                    var url = string.Format("{0}", endPoint);
+                    var response = await client.GetAsync(url);
+
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        
+                        return new Response
+                        {
+                            IsSuccess = false,
+                            Message = response.StatusCode.ToString(),
+                        };
+                    }
+
+                    var result = await response.Content.ReadAsStringAsync();
+                    var list = JsonConvert.DeserializeObject<T>(result);
+
+                    return new Response
+                    {
+                        IsSuccess = true,
+                        Message = "Ok",
+                        Result = list,
+                    };
+
+                }                
+            }
+            catch (Exception ex)
+            {
+
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message,
+                };
+            }
+        }
     }
 }
